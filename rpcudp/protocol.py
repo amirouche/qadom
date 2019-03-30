3e3bee1cb3af7c2b7bb9aebc0187985bf941f26b
Amirouche <amirouche.boubekki@gmail.com>
2019-03-30 12:46:56 +0100

    rpcudp: rework future
    - inherit all exceptions from a base exception
    
    - add NoReplyException and use it in reply's future
    
    - rename self.transport to self._transport
    
    - use procedure registry self.procedure

--- rpcudp/protocol.py
+++ rpcudp/protocol.py
@@ -30,24 +30,38 @@ import msgpack
 log = logging.getLogger(__name__)
 
 
-class MalformedMessage(Exception):
+class RPCUDPException(Exception):
+    pass
+
+
+class MalformedMessage(RPCUDPException):
+    """Message does not contain what is expected.
+
     """
-    Message does not contain what is expected.
+
+
+class NoReplyException(RPCUDPException):
+    """The recipient did not reply.
+
     """
 
 
 class RPCProtocol(asyncio.DatagramProtocol):
     def __init__(self, wait_timeout=5):
-        """
-        @param waitTimeout: Consider it a connetion failure if no response
-        within this time window.
+        """wait_timeout: Consider it a connetion failure if no response within
+        this time window.
+
         """
         self._wait_timeout = wait_timeout
         self._outstanding = {}
-        self.transport = None
+        self._transport = None
+        self.procedures = {}
+
+    def register(self, proc):
+        self.procedures[proc.__name__] = proc
 
     def connection_made(self, transport):
-        self.transport = transport
+        self._transport = transport
 
     def datagram_received(self, datagram, addr):
         log.debug("received datagram from %s", addr)
@@ -68,35 +82,35 @@ class RPCProtocol(asyncio.DatagramProtocol):
 
     def _accept_response(self, uid, data, address):
         try:
-            f, timeout = self._outstanding[uid]
+            future, timeout = self._outstanding[uid]
         except KeyError:
             log.warning("received unknown message %r from %r; ignoring", data, address)
         else:
             log.debug("received response %r for message uid %r from %r", data, uid, address)
             timeout.cancel()
-            f.set_result((True, data))
+            future.set_result(data)
             del self._outstanding[uid]
 
     async def _accept_request(self, uid, data, address):
         # TODO: more validation
         if not isinstance(data, list) or len(data) != 2:
             raise MalformedMessage("Could not read packet: %r" % data)
-        funcname, args = data
-        f = getattr(self, "rpc_%s" % funcname, None)
-        if f is None or not callable(f):
-            msgargs = (self.__class__.__name__, funcname)
+        name, args = data
+        try:
+            procedure = self.procedures[name]
+        except KeyError:
             log.warning("%s has no callable method " "rpc_%s; ignoring request", *msgargs)
-            return
-
-        response = await f(address, *args)
-        log.debug("sending response %s for msg id %s to %s", response, b64encode(uid), address)
-        data = msgpack.packb([uid, b'\x01', response])
-        self.transport.sendto(data, address)
+        else:
+            response = await procedure(address, *args)
+            log.debug("sending response %s for msg id %s to %s", response, b64encode(uid), address)
+            data = msgpack.packb([uid, b'\x01', response])
+            self._transport.sendto(data, address)
 
     def _timeout(self, uid):
         args = (b64encode(uid), self._wait_timeout)
         log.error("Did not received reply for msg id %s within %i seconds", *args)
-        self._outstanding[uid][0].set_result((False, None))
+        future = self._outstanding[uid][0]
+        future.set_exception(NoReplyException())
         del self._outstanding[uid]
 
     def rpc(self, address, name, *args):
@@ -106,7 +120,7 @@ class RPCProtocol(asyncio.DatagramProtocol):
             msg = "Total length message cannot exceed 8K"
             raise MalformedMessage(msg)
         log.debug("calling remote function %s on %s (uid %s)", name, address, b64encode(uid))
-        self.transport.sendto(txdata, address)
+        self._transport.sendto(txdata, address)
 
         loop = asyncio.get_event_loop()
         if hasattr(loop, 'create_future'):
